531dcf913364569704309071dd3fddb96e53c3fa
debgolick <shock1142003@atlanticbb.net>
2017-06-21 16:24:15 -0400

    3=minedminds

--- printtoscreen.rb
+++ printtoscreen.rb
@@ -1,12 +1,13 @@
 1.upto(100) do |i|
   puts i
-end
-upto 100 do [i]
+
+
 string =""
  
-string += "Fizz" if i % 3 == 0
-string += "Buzz" if i % 5 == 0
+string += "mined" if i % 3 == 0
+
+string += "minds" if i % 5 == 0
 
 puts "#{i} = #{string}"
-
 end
+
